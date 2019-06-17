054b3e70ca9f9a067942fc1e60df6d58c12056d4
Zygimantas Kairys <zygi@analogrepublic.com>
2019-06-17 06:58:53 -0700

    chnages to model added easy-peasy

---
+++ model/actions.js
@@ -0,0 +1,12 @@
+import { action } from "easy-peasy";
+
+export default {
+  //   toggle: action((state, id) => {
+  //     state.todos.map(todo => {
+  //       if (todo.id === id) {
+  //         todo.completed = !todo.completed;
+  //       }
+  //       return todo;
+  //     });
+  //   })
+};
---
+++ model/thunk.js
@@ -0,0 +1,12 @@
+import { thunk } from "easy-peasy";
+
+export default {
+  //   characters: [],
+  //   fetchCharacters: thunk(async actions => {
+  //     const res = await fetch(
+  //       ""
+  //     );
+  //     const characters = await res.json();
+  //     actions.setTodos(characters);
+  //   })
+};
